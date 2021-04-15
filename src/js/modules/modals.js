1c25a632426529e1886ce70218a58c52fea1a887
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-04-16 01:47:41 +0500

    trigger an event to close popup window when clicked outside of it

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -11,6 +11,13 @@ const modals = () => {
       modal.style.display = 'none';
       document.body.style.overflow = '';
     });
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+      }
+    });
   }
 
   const callEngineerBtn = document.querySelector('.popup_engineer_btn');
