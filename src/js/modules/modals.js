b1f9ccc072e42d6dd82694d87ce48142733f084c
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-04-16 01:52:57 +0500

    use bootstrap "modal-open" class to toggle visibility of pop-up window

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -5,17 +5,17 @@ const modals = () => {
         e.preventDefault();
       }
       modal.style.display = 'block';
-      document.body.style.overflow = 'hidden';
+      document.body.classList.add('modal-open');
     });
     close.addEventListener('click', () => {
       modal.style.display = 'none';
-      document.body.style.overflow = '';
+      document.body.classList.remove('modal-open');
     });
 
     modal.addEventListener('click', (e) => {
       if (e.target === modal) {
         modal.style.display = 'none';
-        document.body.style.overflow = '';
+        document.body.classList.remove('modal-open');
       }
     });
   }
