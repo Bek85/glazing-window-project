17f2c73397dedc3124b730523a5008819c0c9db9
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-04-16 16:35:53 +0500

    make first instance of tabs display relevant content when clicked

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,40 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+  const header = document.querySelector(headerSelector);
+  const tabs = document.querySelectorAll(tabSelector);
+  const contents = document.querySelectorAll(contentSelector);
+
+  function hideTabContent() {
+    contents.forEach((content) => {
+      content.style.display = 'none';
+    });
+
+    tabs.forEach((tab) => {
+      tab.classList.remove(activeClass);
+    });
+  }
+
+  function showTabContent(i = 0) {
+    contents[i].style.display = 'block';
+    tabs[i].classList.add(activeClass);
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  header.addEventListener('click', (e) => {
+    const target = e.target;
+    if (
+      target.classList.contains(tabSelector.replace(/\./, '')) ||
+      target.parentNode.classList.contains(tabSelector.replace(/\./, ''))
+    ) {
+      tabs.forEach((tab, i) => {
+        if (target == tab || target.parentNode == tab) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+};
+
+export default tabs;
